5918864575dda74f401376fe16ef615d45e07fdd
Herobrine-ymf <ff326787@outlook.com>
2022-01-21 13:38:06 +0800

    Go!

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,6 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+echo 'src-git subconverter https://github.com/tindy2013/openwrt-subconverter.git' >> feeds.conf
+echo 'src-git openclash https://github.com/vernesong/OpenClash.git;dev' >> feeds.conf
--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,7 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+rm -rf package/lean/luci-theme-argon
+git clone -b 18.06 --depth 1 --single-branch https://github.com/jerrykuku/luci-theme-argon.git package/custom/luci-theme-argon
+git clone --depth 1 --single-branch https://github.com/jerrykuku/luci-app-argon-config.git package/custom/luci-app-argon-config
